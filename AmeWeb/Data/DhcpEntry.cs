a883c9d29f64522d232da3598a0323b817b6c88d
Adam Baszynski <adam.baszynski@gmail.com>
2021-04-08 12:29:30 +0200

    AME Web Portal - Initial commit

---
+++ AmeWeb/Data/DhcpEntry.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace AmeDhcp.Data
+{
+    public class DhcpEntry
+    {
+        public string Mac { get; set; }
+        public string Hostname { get; set; }
+        public string ClientIp { get; set; }
+        public IPAddress ClientIpAddress { get; set; }
+        public string Gateway { get; set; }
+    }
+}
---
+++ AmeWeb/Data/DhcpService.cs
@@ -0,0 +1,109 @@
+using IniParser;
+using IniParser.Model;
+using IniParser.Parser;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmeDhcp.Data
+{
+    public class DhcpService
+    {
+        private readonly IConfiguration config;
+        private readonly FileIniDataParser parser;
+
+        public DhcpService(IConfiguration config)
+        {
+            this.config = config;
+            var parserConfig = new IniDataParser();
+            parserConfig.Configuration.AssigmentSpacer = "";
+            parser = new FileIniDataParser(parserConfig);
+        }
+
+        private string ConfigFile => config["DhcpConfigFile"];
+        private IniData DhcpData() => parser.ReadFile(ConfigFile);
+
+        public Task<DhcpEntry[]> GetEntries()
+        {
+            return Task.FromResult(DhcpData().Sections.Where(s => PhysicalAddress.TryParse(s.SectionName, out _)).Select(
+                s => new DhcpEntry
+                {
+                    Mac = s.SectionName,
+                    Hostname = s.Keys["Hostname"] ?? "Unknown",
+                    ClientIp = s.Keys["IPADDR"],
+                    ClientIpAddress = IPAddress.Parse(s.Keys["IPADDR"]),
+                    Gateway = s.Keys["ROUTER_0"]
+                })
+                .ToArray());
+        }
+        public Task<DhcpEntry> GetEntry(string mac)
+        {
+            try
+            {
+                return Task.FromResult(DhcpData().Sections.Where(s => s.SectionName == mac).Select(
+                    s => new DhcpEntry
+                    {
+                        Mac = s.SectionName,
+                        Hostname = s.Keys["Hostname"],
+                        ClientIp = s.Keys["IPADDR"],
+                        ClientIpAddress = IPAddress.Parse(s.Keys["IPADDR"]),
+                        Gateway = s.Keys["ROUTER_0"]
+                    })
+                    .FirstOrDefault());
+            }
+            catch(Exception ex)
+            {
+                return Task.FromResult(new DhcpEntry { Hostname = ex.Message});
+            }
+        }
+
+        public void Update(string mac, string gateway)
+        {
+            IniData data = DhcpData();
+            var clientSection = data.Sections[mac];
+
+            if (gateway == "")
+            {
+                clientSection.RemoveKey("ROUTER_0");
+                clientSection.RemoveKey("DNS_0");
+            }
+            else
+            {
+                clientSection["ROUTER_0"] = gateway;
+                clientSection["DNS_0"] = gateway;
+            }
+            parser.WriteFile(ConfigFile, data, new UTF8Encoding(false));
+        }
+        public void Update(DhcpEntry entry)
+        {
+            IniData data = DhcpData();
+            var clientSection = data.Sections[entry.Mac];
+
+            clientSection["Hostname"] = entry.Hostname;
+            clientSection["IPADDR"] = entry.ClientIp;
+
+            if (entry.Gateway == "")
+            {
+                clientSection.RemoveKey("ROUTER_0");
+                clientSection.RemoveKey("DNS_0");
+            }
+            else
+            {
+                clientSection["ROUTER_0"] = entry.Gateway;
+                clientSection["DNS_0"] = entry.Gateway;
+            }
+            parser.WriteFile(ConfigFile, data, new UTF8Encoding(false));
+        }
+
+        public void Delete(string mac)
+        {
+            IniData data = DhcpData();
+            data.Sections.RemoveSection(mac);
+            parser.WriteFile(ConfigFile, data, new UTF8Encoding(false));
+        }
+    }
+}
